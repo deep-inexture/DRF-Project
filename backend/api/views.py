2cecd702e490f24ecba6c34c7dd128271c662bb1
deep <deep.inexture@gmail.com>
2022-10-12 17:34:40 +0530

    Added new view of first api in views.py and added urls.py file for app url

---
+++ backend/api/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+from django.http import JsonResponse
+
+
+def api_home(request, *args, **kwargs):
+    return JsonResponse({"message": 'This is Django API Response'})
