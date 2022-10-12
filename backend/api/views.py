8b6eec4e1f7aa655a289d24dee9daac6e9f2e8e4
deep <deep.inexture@gmail.com>
2022-10-12 18:24:55 +0530

    Intro to GET request and fetch body of request

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 
 from django.http import JsonResponse
+import json
 
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": 'This is Django API Response'})
+    print(request.GET)
+    print(request.POST)
+    body = request.body                     # byte string of Json data
+    data = {}
+    try:
+        data = json.loads(body)             # String of Json data -> Python Dictionary
+    except:
+        pass
+    print(data.keys())
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers)   # request.Meta
+    data['content_type'] = request.content_type
+    return JsonResponse(data)
