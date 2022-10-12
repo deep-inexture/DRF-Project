b4cde86bd749c834f4e1719e97d2f13a86f69664
deep <deep.inexture@gmail.com>
2022-10-12 17:15:44 +0530

    Updated endpoint to project URL, integrated with server to check django project working status

--- py_client/basic.py
+++ py_client/basic.py
@@ -3,6 +3,7 @@ import requests
 
 endpoint = 'https://httpbin.org'                        # HTTP Request -> HTML Format
 endpoint = 'https://httpbin.org/anything'               # REST API HTTP Request -> Json Format
+endpoint = 'http://127.0.0.1:8000/'                     # Project URL
 
 get_response = requests.get(endpoint)                   # HTTP Request
 
@@ -19,12 +20,13 @@ Content-Type will be application/form-urlencoded
 get_response_form = requests.get(
     endpoint, data={'query': 'Hello Form Data!'})
 
-print(get_response_data.text)                                # Print raw text response - Format: Json
+# print(get_response_data.text)                                # Print raw text response - Format: Json
+print(get_response_form.text)                                # Print raw text response - Format: Json
 
 """ Prints item inside data """
-print(get_response_data.json())                              # Print data in form of Python Dict
+# print(get_response_data.json())                              # Print data in form of Python Dict
 """ Prints item inside form """
-print(get_response_form.json())
+# print(get_response_form.json())
 
 """ Get status_code of the response"""
-print(get_response_data.status_code)
+# print(get_response_data.status_code)
