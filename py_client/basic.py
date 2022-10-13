1759242bb1148abf943c4e97612bf2045df6c7aa
deep <deep.inexture@gmail.com>
2022-10-13 11:43:23 +0530

    updated test code to check usage of model_to_dict in views

--- py_client/basic.py
+++ py_client/basic.py
@@ -3,7 +3,7 @@ import requests
 
 # endpoint = 'https://httpbin.org'                        # HTTP Request -> HTML Format
 # endpoint = 'https://httpbin.org/anything'               # REST API HTTP Request -> Json Format
-endpoint = 'http://127.0.0.1:8000/api'                     # Project URL
+endpoint = 'http://127.0.0.1:8000/api/'                     # Project URL
 
 get_response = requests.get(endpoint)                   # HTTP Request
 
@@ -18,6 +18,7 @@ Content-Type will be application/form-urlencoded
 """
 # get_response_form = requests.get(endpoint, data={'query': 'Hello Form Data!'})
 
+# print(get_response_data.headers)
 # print(get_response_data.text)                                # Print raw text response - Format: Json
 # print(get_response_form.text)                                # Print raw text response - Format: Json
 
