e5e7e31d064ea8bf7e5d4c3c58a2eb67e01fc067
deep <deep.inexture@gmail.com>
2022-10-13 13:24:37 +0530

    Looked at the POST method of api_view with serializer

--- backend/api/views.py
+++ backend/api/views.py
@@ -10,15 +10,13 @@ from rest_framework.decorators import api_view
 from products.serializers import ProductSerializer
 
 
-@api_view(['GET'])
+@api_view(['POST'])
 def api_home(request, *args, **kwargs):
     """
     DRF api_view
     """
-    instance = Product.objects.all().order_by("?").first()
-    data = {}
-    if instance:
-        # data = model_to_dict(model_data, fields=['id', 'title', 'price', 'sale_price'])
-        data = ProductSerializer(instance).data
-        print(data)
-    return Response(data)
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        # data = serializer.save()
+        print(serializer.data)
+        return Response(serializer.data)
