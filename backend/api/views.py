e5b8147b36c15a9695c6748b42ab002b72098f2e
deep <deep.inexture@gmail.com>
2022-10-13 11:44:02 +0530

    Added models_to_views library to discover usage of json data to return into python dict

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,18 +1,17 @@
 from django.shortcuts import render
 
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 import json
 
 from products.models import Product
+from django.forms.models import model_to_dict
 
 
 def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
-        data['title'] = model_data.title
-        data['content'] = model_data.content
-        data['price'] = model_data.price
+        data = model_to_dict(data, fields=['id', 'title', 'price'])
         """
         model instance (model_data) turn a python dict to return Json to client
         """
