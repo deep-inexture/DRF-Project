7819f05c23e4660cd8ae49bc6399b3c1cc9b265e
deep <deep.inexture@gmail.com>
2022-10-13 17:55:13 +0530

    updated url for function based api_view

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.ProductListCreateAPIView.as_view()),
     path('<int:pk>/', views.ProductDetailAPIView.as_view())
+    # path('', views.product_alt_view),
+    # path('<int:pk>/', views.product_alt_view)
 ]
