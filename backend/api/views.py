7cf5a01a663faf00be942a7d4d6d0419f138290b
deep <deep.inexture@gmail.com>
2022-10-13 12:12:43 +0530

    Learned api_view decorator from rest_framework

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,18 +1,20 @@
+from django.forms import model_to_dict
 from django.shortcuts import render
 
-from django.http import JsonResponse, HttpResponse
 import json
 
 from products.models import Product
-from django.forms.models import model_to_dict
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 
+@api_view(['GET'])
 def api_home(request, *args, **kwargs):
+    """
+    DRF api_view
+    """
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
-        data = model_to_dict(data, fields=['id', 'title'])
-        """
-        model instance (model_data) turn a python dict to return Json to client
-        """
-    return JsonResponse(data)
+        data = model_to_dict(model_data, fields=['id', 'title', 'price'])
+    return Response(data)
