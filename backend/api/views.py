d8b795d3ab4080947ed940bb5c124842cba6afd1
deep <deep.inexture@gmail.com>
2022-10-13 12:44:33 +0530

    Updated views with use of serializer instead of model_to_dict from rest_framework

--- backend/api/views.py
+++ backend/api/views.py
@@ -7,14 +7,18 @@ from products.models import Product
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+from products.serializers import ProductSerializer
+
 
 @api_view(['GET'])
 def api_home(request, *args, **kwargs):
     """
     DRF api_view
     """
-    model_data = Product.objects.all().order_by("?").first()
+    instance = Product.objects.all().order_by("?").first()
     data = {}
-    if model_data:
-        data = model_to_dict(model_data, fields=['id', 'title', 'price'])
+    if instance:
+        # data = model_to_dict(model_data, fields=['id', 'title', 'price', 'sale_price'])
+        data = ProductSerializer(instance).data
+        print(data)
     return Response(data)
