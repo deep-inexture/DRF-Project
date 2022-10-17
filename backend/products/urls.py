0e5a6a29c27b689fb934e7d9887ea6cbf640811b
deep <deep.inexture@gmail.com>
2022-10-17 15:00:46 +0530

    Updated urls back to generic class based views url

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -3,12 +3,12 @@ from . import views
 
 
 urlpatterns = [
-    # path('', views.ProductListCreateAPIView.as_view()),                 # Changing to Product Mixin View
-    path('', views.ProductMixinView.as_view()),
+    path('', views.ProductListCreateAPIView.as_view()),                 # Changing to Product Mixin View
+    # path('', views.ProductMixinView.as_view()),
     path('<int:pk>/update/', views.ProductUpdateAPIView.as_view()),
     path('<int:pk>/delete/', views.ProductDeleteAPIView.as_view()),
-    # path('<int:pk>/', views.ProductDetailAPIView.as_view()),             # Changing to Product mixin view class
-    path('<int:pk>/', views.ProductMixinView.as_view())
+    path('<int:pk>/', views.ProductDetailAPIView.as_view()),             # Changing to Product mixin view class
+    # path('<int:pk>/', views.ProductMixinView.as_view())
     # path('', views.product_alt_view),
     # path('<int:pk>/', views.product_alt_view)
 ]
