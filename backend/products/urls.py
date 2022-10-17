06b15dc7b872b7ebc011cff2dd07f20e021e8481
deep <deep.inexture@gmail.com>
2022-10-17 11:12:15 +0530

    Added new urls for Generic Update and Destroy API

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.ProductListCreateAPIView.as_view()),
+    path('<int:pk>/update/', views.ProductUpdateAPIView.as_view()),
+    path('<int:pk>/delete/', views.ProductDeleteAPIView.as_view()),
     path('<int:pk>/', views.ProductDetailAPIView.as_view())
     # path('', views.product_alt_view),
     # path('<int:pk>/', views.product_alt_view)
